611486ec7a58f8224607cba1f2b292eb0e20dba8
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-05-15 09:45:42 -0500

    seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,9 @@
 const seedUsers = require('./user-seeds');
 const seedEvents = require('./event-seeds');
-// const seedComments = require('./comment-seeds');
-// const seedVotes = require('./vote-seeds');
+const seedComments = require('./comment-seeds');
+const seedRSVP_Yes = require('./rsvp_yes-seeds');
+const seedRSVP_Interested = require('./rsvp_interested-seeds');
+
 
 const sequelize = require('../config/connection');
 
@@ -13,13 +15,16 @@ const seedAll = async () => {
 
   await seedEvents();
   console.log('--------------');
-/*
+
   await seedComments();
   console.log('--------------');
 
-  await seedVotes();
+  await seedRSVP_Yes();
   console.log('--------------');
-*/
+
+  await seedRSVP_Interested();
+  console.log('--------------');
+
   process.exit(0);
 };
 
