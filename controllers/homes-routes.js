391e702ec8f63395663ee82ab1f01eff66b7181e
Thomas Ruppeiner <truppeiner@gmail.com>
2022-05-17 18:45:59 -0500

    handlebars templating/home route for directory

--- controllers/homes-routes.js
+++ controllers/homes-routes.js
@@ -2,9 +2,34 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Comment, Event, RSVP_Yes, RSVP_Interested} = require('../models');
 
-// return data
+// render directory with event information
 router.get('/', (req,res) =>{
-    res.render('directory');
+    Event.findAll({
+        attributes: [
+            'event_name',
+            'event_description',
+            'event_location',
+            'event_date',
+            'event_start_time',
+            'event_end_time'
+        ],
+        include: [
+            {
+                model: User,
+                attributes:['username']
+            }
+        ]
+    })
+    .then(dbEventData => {
+        // console.log(dbEventData);
+        const events = dbEventData.map(event => event.get({ plain: true }))
+        console.log(events)
+    res.render('directory', { events });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 // // login route
