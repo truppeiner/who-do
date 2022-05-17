02c53feab8c8213cf669b2102887197831dfb85b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-05-17 13:50:50 -0500

    update event model to accomodate rsvp_yes, rsvp_interested

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -72,9 +72,9 @@ router.post('/', (req, res) => {
             req.session.user_id = dbUserData.id;
             req.session.username = dbUserData.username;
             req.session.loggedIn = true;
-
             res.json(dbUserData);
         });
+        res.json(dbUserData);
     })
     .catch(err => {
         console.log(err);
--- models/Event.js
+++ models/Event.js
@@ -2,8 +2,76 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 // create Event model
 class Event extends Model {
-  
-
+  static rsvp_yes(body, models) {
+    return models.RSVP_Yes.create({
+      user_id: body.user_id,
+      event_id: body.event_id
+    }).then(() => {
+      return Event.findOne({
+        where: {
+          id: body.event_id
+        },
+        attributes: [
+          'id',
+          'event_name',
+          'event_description',
+          'event_location',
+          'event_date',
+          'event_start_time',
+          'event_end_time',
+          'event_url',
+          'user_id',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM rsvp_yes WHERE event.id = rsvp_yes.event_id)'), 'rsvp_yes_count']
+        ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: ['id', 'comment_text', 'event_id', 'user_id', 'created_at'],
+            include: {
+              model: models.User,
+              attributes: ['username']
+            }
+          }
+        ]
+      });
+    });
+  }
+  static rsvp_interested(body, models) {
+    return models.RSVP_Interested.create({
+      user_id: body.user_id,
+      event_id: body.event_id
+    }).then(() => {
+      return Event.findOne({
+        where: {
+          id: body.event_id
+        },
+        attributes: [
+          'id',
+          'event_name',
+          'event_description',
+          'event_location',
+          'event_date',
+          'event_start_time',
+          'event_end_time',
+          'event_url',
+          'user_id',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM rsvp_interested WHERE event.id = rsvp_interested.event_id)'), 'rsvp_interested_count']
+        ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: ['id', 'comment_text', 'event_id', 'user_id', 'created_at'],
+            include: {
+              model: models.User,
+              attributes: ['username']
+            }
+          }
+        ]
+      });
+    });
+  }
 }
 
 // create fields/columns for Event model
@@ -46,13 +114,6 @@ Event.init(
       allowNull: true,
       validate: {
         isURL: true
-      }
-    },
-    user_name: {
-      type: DataTypes.STRING,
-      references: {
-        model: 'user',
-        key: id
       }
     },
     user_id: {
