0630b56137e7a7d11e5de065ea7a641689760cf3
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-05-18 20:08:57 -0500

    helpers.js

--- server.js
+++ server.js
@@ -22,11 +22,9 @@ const sess = {
 
 app.use(session(sess));
 
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ 
-  // helpers 
-});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
